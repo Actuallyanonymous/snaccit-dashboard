3e5044900ff9cf525b015454126907fdbc3f7919
Pushkin Mangla <mymac@Pushkins-MacBook-Pro-1745.local>
2025-12-24 01:38:02 +0530

    Changing the storagebucket name

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDDFCPcfBKcvrkjqidsXstHqe8Og_3u36k",
   authDomain: "snaccit-7d853.firebaseapp.com",
   projectId: "snaccit-7d853",
-  storageBucket: "snaccit-7d853.appspot.com",
+  storageBucket: "snaccit-7d853.firebasestorage.app",
   messagingSenderId: "523142849231",
   appId: "1:523142849231:web:f10e23785d6451f510cdba"
 };
